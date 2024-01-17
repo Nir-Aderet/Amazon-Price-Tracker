b7838d9e9c06db7b3ef33d5004f7ddcf4be7e4c0
Nir-Aderet <128510850+Nir-Aderet@users.noreply.github.com>
2024-01-17 15:26:11 +0200

    Update notification.py

--- notification.py
+++ notification.py
@@ -2,20 +2,20 @@ import smtplib
 
 def send_mail(url, title, sender_email):
  try:
-        """SMTP client session object that can be used to send mail to any internet machine
-        with an SMTP or ESMTP listener daemon."""
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-
-        server.login('nirisworking@gmail.com', 'skge ehbm wdgr loaj')
-
-        subject = 'An Item you requested has a lower price!'
-        body = 'The item ' + title + ' is on sale!\nCheck this link for more information: ' + url
-        msg = f"subject: {subject}\n\n{body}"
-
-        server.sendmail('nirisworking@gmail.com', 'nirisworking@gmail.com', msg)
-        print('A massage has been sent')
-    finally:
-        server.quit()
+         """SMTP client session object that can be used to send mail to any internet machine
+         with an SMTP or ESMTP listener daemon."""
+         server = smtplib.SMTP('smtp.gmail.com', 587)
+         server.ehlo()
+         server.starttls()
+         server.ehlo()
+ 
+         server.login('nirisworking@gmail.com', 'skge ehbm wdgr loaj')
+ 
+         subject = 'An Item you requested has a lower price!'
+         body = 'The item ' + title + ' is on sale!\nCheck this link for more information: ' + url
+         msg = f"subject: {subject}\n\n{body}"
+ 
+         server.sendmail('nirisworking@gmail.com', 'nirisworking@gmail.com', msg)
+         print('A massage has been sent')
+     finally:
+         server.quit()

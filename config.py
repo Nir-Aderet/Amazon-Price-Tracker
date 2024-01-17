9175ada23e57a26e63642d7fc99830ab4924b22f
Nir-Aderet <128510850+Nir-Aderet@users.noreply.github.com>
2024-01-17 15:29:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
+
+# urls for the items we wish to price track; Here is where you add the items to be price tracked
+DATA = {'URL': [
+    'https://www.amazon.com/Upgrade-Cooling-KeiBn-15-6-17-3-Laptops/dp/B09NRTVV3M/ref=sr_1_12?crid=2Q8J5K4JPYHFO' \
+    '&keywords=cooling%2Bfan%2Bfor%2Blaptop&qid=1695706802&refinements=p_n_is_free_shipping%3A10236242011%2Cp_72' \
+    '%3A1248879011&rnid=1248877011&s=electronics&sprefix=cooling%2Bfan%2Bfor%2Bla%2Caps%2C405&sr=1-12&th=1 ',
+    'https://www.amazon.com/Name-Wind-Anniversary-Kingkiller-Chronicle/dp/0756413710/ref=sr_1_6?crid=PIJPJ5TJS9C'
+    '&keywords=patrick+rothfuss&qid=1695729567&sprefix=patri%2Caps%2C374&sr=8-6',
+    'https://www.amazon.com/Regard-Silent-Things-Kingkiller-Chronicle/dp/0756411327/ref=d_pd_sbs_sccl_3_3/133-3715102'
+    '-2100713?pd_rd_w=Hi50N&content-id=amzn1.sym.83180963-0377-46ea-9aa4-1ece655ef63e&pf_rd_p=83180963-0377-46ea-9aa4'
+    '-1ece655ef63e&pf_rd_r=14MYF3HCVSDR5W31F2PF&pd_rd_wg=tcsu4&pd_rd_r=5dd15f65-49a0-46c4-bd30-68115103a743&pd_rd_i'
+    '=0756411327&psc=1']}
+
+#  what your web browser is sending in the "User-Agent" header for your HTTP requests
 HEADERS = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/116.0.0.0 Safari/537.36'}
 
-DATA = {'URL': [
-    'https://www.amazon.com/...',
-    'https://www.amazon.com/...',
-    # Add more URLs here
-]}
 
-GMAIL_CREDENTIALS = {
-    'email': 'nirisworking@gmail.com',
-    'password': 'skge ehbm wdgr loaj'
-}

e18cd585428a3c999b84b0e143bb1cb3bd68a2fc
Nir-Aderet <128510850+Nir-Aderet@users.noreply.github.com>
2024-01-17 15:27:04 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+HEADERS = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/116.0.0.0 Safari/537.36'}
+
+DATA = {'URL': [
+    'https://www.amazon.com/...',
+    'https://www.amazon.com/...',
+    # Add more URLs here
+]}
+
+GMAIL_CREDENTIALS = {
+    'email': 'nirisworking@gmail.com',
+    'password': 'skge ehbm wdgr loaj'
+}

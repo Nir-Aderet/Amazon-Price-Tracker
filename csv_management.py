9870f54ac53fa0186e8f2af42f100186b82fa00a
Nir-Aderet <128510850+Nir-Aderet@users.noreply.github.com>
2024-01-17 15:37:49 +0200

    Update csv_management.py

--- csv_management.py
+++ csv_management.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
 def check_price(file_path):
-    df = pd.read_csv(file_path)
-    next(df)
-    for row in df:
-        if row[2] <= row[3]:
-            send_mail(row[0], row[1])
-            print('An email about ' + row[1] + ' price has been sent')
+    try:
+        df = pd.read_csv(file_path)
+        next(df)
+        for row in df:
+            if row[2] <= row[3]:
+                send_mail(row[0], row[1])
+                print('An email about ' + row[1] + ' price has been sent')
+    except FileNotFoundError as e:
+        print(f"Error: File not found - {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")

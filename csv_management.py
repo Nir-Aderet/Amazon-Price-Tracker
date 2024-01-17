a7ee7f6f553c8315838629f94a6bd55e93d5e295
Nir-Aderet <128510850+Nir-Aderet@users.noreply.github.com>
2024-01-17 15:25:19 +0200

    Update csv_management.py

--- csv_management.py
+++ csv_management.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 def check_price(file_path, sender_email, sender_password):
-    # ... (your existing implementation)
+    df = pd.read_csv(file_path)
+    next(df)
+    for row in df:
+        if row[2] <= row[3]:
+            send_mail(row[0], row[1])
+            print('An email about ' + row[1] + ' price has been sent')

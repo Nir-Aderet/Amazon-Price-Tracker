325ea1b2b8ef455fdf3587235fc6d59ccc972a06
Nir-Aderet <128510850+Nir-Aderet@users.noreply.github.com>
2024-01-17 15:23:30 +0200

    Update notification.py

--- notification.py
+++ notification.py
@@ -1,4 +1,21 @@
 import smtplib
 
-def send_mail(url, title, sender_email, sender_password):
-    # ... (your existing implementation)
+def send_mail(url, title, sender_email):
+ try:
+        """SMTP client session object that can be used to send mail to any internet machine
+        with an SMTP or ESMTP listener daemon."""
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+
+        server.login('nirisworking@gmail.com', 'skge ehbm wdgr loaj')
+
+        subject = 'An Item you requested has a lower price!'
+        body = 'The item ' + title + ' is on sale!\nCheck this link for more information: ' + url
+        msg = f"subject: {subject}\n\n{body}"
+
+        server.sendmail('nirisworking@gmail.com', 'nirisworking@gmail.com', msg)
+        print('A massage has been sent')
+    finally:
+        server.quit()
